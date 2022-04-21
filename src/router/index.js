6f74460d152b9c37db2241a36015d9e131606573
Davis Peck <Dpeck1997@gmail.com>
2022-04-21 13:54:59 -0700

    init and creating a sign up and login to the frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/SignUp',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
